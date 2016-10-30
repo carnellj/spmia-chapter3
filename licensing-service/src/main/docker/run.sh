4904a03fbc7be65b9d7fb394127dc06bdc3a43d4
carnellj <john.carnell@inin.com>
2016-10-30 19:24:30 -0400

    Modified docker build files for chapter 3

--- licensing-service/src/main/docker/run.sh
+++ licensing-service/src/main/docker/run.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
+getPort() {
+    echo $1 | cut -d : -f 3 | xargs basename
+}
 
 echo "********************************************************"
-echo "Waiting for the configuration server to start"         *
+echo "Waiting for the configuration server to start on port $(getPort $CONFIGSERVER_PORT)"
 echo "********************************************************"
-while ! `nc -z configserver 8888`; do sleep 3; done
+while ! `nc -z configserver  $(getPort $CONFIGSERVER_PORT)`; do sleep 3; done
 echo ">>>>>>>>>>>> Configuration Server has started"
 
 echo "********************************************************"
-echo "Waiting for the database server to start"              *
+echo "Waiting for the database server to start on port $(getPort $DATABASE_PORT)"
 echo "********************************************************"
-while ! `nc -z database 5432`; do sleep 3; done
+while ! `nc -z database $(getPort $DATABASE_PORT)`; do sleep 3; done
 echo ">>>>>>>>>>>> Database Server has started"
 
 
 echo "********************************************************"
 echo "Starting License Server with Configuration Service :  $CONFIGSERVER_URI";
 echo "********************************************************"
-java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/licensingservice/licensing-service-0.0.1-SNAPSHOT.jar
+java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/licensingservice/@project.build.finalName@.jar
