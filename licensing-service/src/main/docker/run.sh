f55ce204a4962309bfa983c1d105377babf20c70
carnellj <john.carnell@inin.com>
2017-01-08 13:21:41 -0500

    Fixed docker version and travis scripts

--- licensing-service/src/main/docker/run.sh
+++ licensing-service/src/main/docker/run.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 
-getPort() {
-    echo $1 | cut -d : -f 3 | xargs basename
-}
-
 echo "********************************************************"
-echo "Waiting for the configuration server to start on port $(getPort $CONFIGSERVER_PORT)"
+echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
 echo "********************************************************"
-while ! `nc -z configserver  $(getPort $CONFIGSERVER_PORT)`; do sleep 3; done
+while ! `nc -z configserver $CONFIGSERVER_PORT `; do sleep 3; done
 echo ">>>>>>>>>>>> Configuration Server has started"
 
 echo "********************************************************"
-echo "Waiting for the database server to start on port $(getPort $DATABASE_PORT)"
+echo "Waiting for the database server to start on port $DATABASESERVER_PORT"
 echo "********************************************************"
-while ! `nc -z database $(getPort $DATABASE_PORT)`; do sleep 3; done
+while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
 echo ">>>>>>>>>>>> Database Server has started"
-
 
 echo "********************************************************"
 echo "Starting License Server with Configuration Service :  $CONFIGSERVER_URI";
