d9d52d7012c6c6598ebee85a8075705b2889ef07
carnellj <john.carnell@inin.com>
2017-04-08 10:21:10 -0400

    Latest edits

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-
-@EnableAutoConfiguration
 @SpringBootApplication
 @RefreshScope
 public class Application {
